1841062969da979124a600c1bcf432014ef5c2f8
kobimm1 <52923025+kobimm1@users.noreply.github.com>
2021-08-13 13:26:00 +0300

    Update welcome.groovy
    add function

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,4 +1,8 @@
 def call (name){
   echo "Hey ${name}, How are you?"
-  echo "BUILD_NUMBER = ${env.BUILD_NUMBER}"
+  getBuildNumber()
+}
+
+def getBuildNumber(){
+ echo "BUILD_NUMBER = ${env.BUILD_NUMBER}"
 }
