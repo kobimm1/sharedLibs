436b3b3dcb7007ab7e8c251bafcc6079a9dddef8
kobimm1 <52923025+kobimm1@users.noreply.github.com>
2021-08-13 13:35:32 +0300

    Update welcome.groovy
    add build url

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,9 +1,14 @@
 def call (name){
   echo "Hey ${name}, How are you?"
   getBuildNumber()
+  getBuildURL()
 }
 
 def getBuildNumber(){
   echo "${env.CUSTOME_MESSAGE}"
  echo "BUILD_NUMBER = ${env.BUILD_NUMBER}"
+}
+
+def getBuildURL(){
+  echo "${env.BUILD_URL}"
 }
